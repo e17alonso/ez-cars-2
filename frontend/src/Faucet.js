232bcc2aea0af2d268c3d04555a514e4f1455adc
e17alonso <rolandoalonso@ufm.edu>
2024-11-20 17:27:04 -0600

    editar faucet.js

--- frontend/src/Faucet.js
+++ frontend/src/Faucet.js
@@ -1,14 +1,13 @@
 // frontend/src/Faucet.js
 import React, { useContext, useState } from 'react';
 import axios from 'axios';
-import { Button, Typography, TextField, Box, CircularProgress } from '@mui/material';
+import { Button, Typography, TextField, Box } from '@mui/material';
 import { Web3Context } from './Web3Context';
 
 function Faucet() {
   const { currentAccount } = useContext(Web3Context);
   const [message, setMessage] = useState('');
   const [amount, setAmount] = useState('');
-  const [loading, setLoading] = useState(false); // Estado de carga
 
   const requestTokens = async () => {
     if (!amount || isNaN(amount) || amount <= 0) {
@@ -16,20 +15,15 @@ function Faucet() {
       return;
     }
 
-    setLoading(true); // Iniciar carga
-    setMessage(''); // Limpiar mensajes anteriores
-
     try {
-      const response = await axios.post(`${process.env.REACT_APP_BACKEND_URL}/api/faucet`, {
+      const response = await axios.post('https://ez-cars-2-39214c762f0e.herokuapp.com/api/faucet', {
         toAddress: currentAccount,
         amount: amount, // Cantidad especificada por el usuario
       });
       setMessage(`Tokens enviados. Hash de la transacción: ${response.data.txHash}`);
     } catch (error) {
       console.error('Error al solicitar tokens:', error);
-      setMessage('Error al solicitar tokens. Inténtalo nuevamente.');
-    } finally {
-      setLoading(false); // Finalizar carga
+      setMessage('Error al solicitar tokens');
     }
   };
 
@@ -48,31 +42,10 @@ function Faucet() {
         fullWidth
         margin="normal"
         type="number"
-        disabled={loading} // Deshabilitar campo durante la carga
       />
-      <Box sx={{ position: 'relative', display: 'inline-flex' }}>
-        <Button
-          variant="contained"
-          color="primary"
-          onClick={requestTokens}
-          disabled={loading} // Deshabilitar botón durante la carga
-        >
-          Solicitar PD
-        </Button>
-        {loading && (
-          <CircularProgress
-            size={24}
-            sx={{
-              color: 'primary.main',
-              position: 'absolute',
-              top: '50%',
-              left: '50%',
-              marginTop: '-12px',
-              marginLeft: '-12px',
-            }}
-          />
-        )}
-      </Box>
+      <Button variant="contained" color="primary" onClick={requestTokens}>
+        Solicitar PD
+      </Button>
       {message && (
         <Typography variant="body2" color="textSecondary" sx={{ marginTop: 2 }}>
           {message}
